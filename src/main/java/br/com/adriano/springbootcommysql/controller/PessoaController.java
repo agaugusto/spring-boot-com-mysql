e337f4907856225e183b83bd2322b2b195da3f0f
adriano <adrianogaugusto@gmail.com>
2020-08-01 01:10:42 -0300

    Exercício finalizado

--- src/main/java/br/com/adriano/springbootcommysql/controller/PessoaController.java
+++ src/main/java/br/com/adriano/springbootcommysql/controller/PessoaController.java
@@ -1,12 +1,13 @@
 package br.com.adriano.springbootcommysql.controller;
 
+import br.com.adriano.springbootcommysql.controller.dto.PessoaRq;
 import br.com.adriano.springbootcommysql.controller.dto.PessoaRs;
+import br.com.adriano.springbootcommysql.model.Pessoa;
 import br.com.adriano.springbootcommysql.repository.PessoaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,5 +28,34 @@ public class PessoaController {
                 .stream()
                 .map((p) -> PessoaRs.converter(p))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public PessoaRs findById(@PathVariable("id") Long id){
+        Pessoa pessoa = pessoaRepository.getOne(id);
+        return PessoaRs.converter(pessoa);
+    }
+
+    @PostMapping("/")
+    public void savePerson(@RequestBody PessoaRq pessoa){
+        var p = new Pessoa();
+        p.setNome(pessoa.getNome());
+        p.setSobrenome(pessoa.getSobrenome());
+        pessoaRepository.save(p);
+
+    }
+
+    @PutMapping("/{id}")
+    public void updatePerson(@PathVariable("id") Long id, @RequestBody PessoaRs pessoa) throws Exception {
+        var p = pessoaRepository.findById(id);
+
+        if (p.isPresent()){
+            var pessoaSave = p.get();
+            pessoaSave.setNome(pessoa.getNome());
+            pessoaSave.setSobrenome(pessoa.getSobrenome());
+            pessoaRepository.save(pessoaSave);
+        } else {
+            throw new Exception("Pessoa não encontrada!");
+        }
     }
 }
--- src/main/java/br/com/adriano/springbootcommysql/controller/dto/PessoaRq.java
+++ src/main/java/br/com/adriano/springbootcommysql/controller/dto/PessoaRq.java
@@ -2,27 +2,10 @@ package br.com.adriano.springbootcommysql.controller.dto;
 
 import br.com.adriano.springbootcommysql.model.Pessoa;
 
-public class PessoaRs {
+public class PessoaRq {
 
-    private Long id;
     private String nome;
     private String sobrenome;
-
-    public static PessoaRs converter(Pessoa p){
-        var pessoa = new PessoaRs();
-        pessoa.setId(p.getId());
-        pessoa.setNome(p.getNome());
-        pessoa.setSobrenome(p.getSobrenome());
-        return pessoa;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
